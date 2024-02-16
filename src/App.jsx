c35730d0fd258f75e6e2929f6232eb46b3ebc345
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-16 16:00:39 +0600

    learn about react conditional rendering with class component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+import ConditionalRendering from './Components'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <ConditionalRendering></ConditionalRendering>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/index.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import HomePage from './HomePage'
+import LogInPage from './LogInPage'
+
+export default class ConditionalRendering extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+    render() {
+        const { isLoggedIn } = this.state
+        // 1 way of conditional rendering (using if-else)---
+        // if (this.state.isLoggedIn) {
+        //     return <HomePage></HomePage>
+        // }
+        // else {
+        //     return <LogInPage></LogInPage>
+        // }
+        // -------------------------------
+
+        // 2 or second way of conditional rendering (using element variable)---
+        // let element;
+        // if(isLoggedIn){
+        //     element = <HomePage></HomePage>
+        // }
+        // else{
+        //     element = <LogInPage></LogInPage>
+        // }
+        // return (
+        //     <div>
+        //         {element}
+        //     </div>
+        // )
+        // --------------------------------------
+
+        // 3 or third way of conditional rendering (using ternary operator)---
+        // return (isLoggedIn ? <HomePage></HomePage> : <LogInPage></LogInPage>)
+        //    --------------------------------------------
+        
+        // 4 or forth way of conditional rendering (using ternary operator)---
+        let element;
+        element = isLoggedIn ? <HomePage></HomePage> : <LogInPage></LogInPage>
+        return (
+            <div>
+                {element}
+            </div>
+        )
+    }
+}
